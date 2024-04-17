6d968e1a38c6ed30e4ada1eedf88c940d2df1aa4
Martins <martins.ikpe@stu.cu.edu.ng>
2024-04-17 20:09:20 +0100

    more products

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -73,12 +73,72 @@ const page = () => {
             <div>   <span className="service-name text-center">Our Products</span>
                 <h2 className="title text-center">Purchase any of our numerous vehicles</h2>
             </div>
-            <div className="grid grid-cols-3">
+            <div className="grid grid-cols-3 gap-[50px]">
                 <PricingCard
-                    name="Red Ferrari"
-                    title="Always arrive in stile"
-                    price="1400"
-                    image="https://images.pexels.com/photos/337909/pexels-photo-337909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1https://images.pexels.com/photos/337909/pexels-photo-337909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                    name="Toyota Camry"
+                    title="The Toyota Camry is a reliable and comfortable midsize sedan known for its spacious interior, smooth ride, and fuel efficiency. Equipped with advanced safety features and modern technology, the Camry offers a perfect blend of practicality and style."
+                    price="25000"
+                    image="https://images.pexels.com/photos/18186239/pexels-photo-18186239/free-photo-of-taxi-standing-in-front-of-a-crosswalk-with-the-dubai-skyline-in-the-background.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                    btnText="Purchase"
+                    titleOne="Sofas, couches, and sectionals"
+                    titleTwo="Dining tables and chairs"
+                    titleThree="Beds and mattresses"
+                    titleFour="Cabinets and storage solutions"
+
+                />
+                <PricingCard
+                    name="Honda CR-V"
+                    title="Description: The Honda CR-V is a versatile compact SUV renowned for its practicality, reliability, and spacious cabin. With ample cargo space, comfortable seating, and advanced safety features, the CR-V is an ideal choice for families and adventurers alike."
+                    price="28000"
+                    image="https://images.pexels.com/photos/6894420/pexels-photo-6894420.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                    btnText="Purchase"
+                    titleOne="Sofas, couches, and sectionals"
+                    titleTwo="Dining tables and chairs"
+                    titleThree="Beds and mattresses"
+                    titleFour="Cabinets and storage solutions"
+
+                />
+                <PricingCard
+                    name="Ford F-150"
+                    title="The Ford F-150 is a legendary full-size pickup truck known for its robust performance, towing capability, and innovative features. With multiple engine options, spacious cabin, and rugged design, the F-150 is perfect for work or play."
+                    price="35000"
+                    image="https://images.pexels.com/photos/2791685/pexels-photo-2791685.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                    btnText="Purchase"
+                    titleOne="Sofas, couches, and sectionals"
+                    titleTwo="Dining tables and chairs"
+                    titleThree="Beds and mattresses"
+                    titleFour="Cabinets and storage solutions"
+
+                />
+                <PricingCard
+                    name="Tesla Model 3"
+                    title="The Tesla Model 3 is an all-electric compact sedan that offers exhilarating performance, cutting-edge technology, and zero emissions. With instant acceleration, long-range capability, and advanced autopilot features, the Model 3 redefines the driving experience."
+                    price="45000"
+                    image="https://images.pexels.com/photos/11099564/pexels-photo-11099564.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                    btnText="Purchase"
+                    titleOne="Sofas, couches, and sectionals"
+                    titleTwo="Dining tables and chairs"
+                    titleThree="Beds and mattresses"
+                    titleFour="Cabinets and storage solutions"
+
+                />
+                <PricingCard
+                    name="Chevrolet Silverado"
+                    title="The Chevrolet Silverado is a tough and capable full-size pickup truck designed to handle the toughest jobs. With powerful engine options, spacious cabin, and innovative towing features, the Silverado is ready for any task."
+                    price="30000"
+                    image="https://images.pexels.com/photos/12295402/pexels-photo-12295402.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                    btnText="Purchase"
+                    titleOne="Sofas, couches, and sectionals"
+                    titleTwo="Dining tables and chairs"
+                    titleThree="Beds and mattresses"
+                    titleFour="Cabinets and storage solutions"
+
+                />
+                <PricingCard
+                    name="BMW 3 Series"
+                    title="The BMW 3 Series is a luxury compact sedan known for its sporty performance, refined interior, and sophisticated styling. With precise handling, powerful engine options, and advanced technology, the 3 Series offers an exhilarating driving experience."
+                    price="40000"
+                    image="https://images.pexels.com/photos/14776717/pexels-photo-14776717.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                     btnText="Purchase"
                     titleOne="Sofas, couches, and sectionals"
                     titleTwo="Dining tables and chairs"
